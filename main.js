e94d0b1aeb639244219e9efa3e89a5aca6341823
KenYuChang <justin0915justin0915@gmail.com>
2023-03-03 16:52:17 +0800

    Initial project

---
+++ main.js
@@ -0,0 +1,16 @@
+let saveEl = document.getElementById("save-el");
+
+let countEl = document.getElementById("count-el");
+console.log(saveEl);
+let count = 0;
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+function save() {
+  let countStr = count + " - ";
+  //textContent can reed the space before '-', so we will not use innerText
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
